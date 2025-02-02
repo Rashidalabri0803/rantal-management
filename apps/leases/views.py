af4826af667f353b604242772e0e6580bf88bd5d
Rashidalabri0803 <rashid92759346@gmail.com>
2025-02-02 14:52:37 +0000

    ee

--- apps/leases/views.py
+++ apps/leases/views.py
@@ -2,14 +2,11 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
-from tinycss2.color4 import parse_color
 from weasyprint import HTML
 
 from .forms import LeaseForm
 from .models import Lease
 
-color = parse_color('rgba(255, 99, 71, 0.5)')
-print(color)
 
 def lease_list(request):
     leases = Lease.objects.all()
@@ -35,6 +32,7 @@ def generate_lease_pdf(request, lease_id):
     context = {'lease': lease}
     html_context = render_to_string('leases/lease_template.html', context)
     pdf_file = HTML(string=html_context).write_pdf()
+
     response = HttpResponse(pdf_file, content_type='application/pdf')
     response['Content-Disposition'] = f'filename="lease_{lease.id}.pdf'
     return response
